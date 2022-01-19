c430d9b9f626d553b3eb6def862128a6d1402a40
IFloresCh <iflores@institumvm.cat>
2022-01-19 15:39:08 +0100

    upload

--- UF2-actividad1/src/main.c
+++ UF2-actividad1/src/main.c
@@ -1,8 +1,10 @@
+
 #include <stdio.h>
-
-int main()
-{
-	printf("Hello World!\n");
-
-	return (0);
+#include "function.h"
+void main(){
+    int n;
+    n= validate();
+    
+   // printf("you introduced %d",n);
 }
+
