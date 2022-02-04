2f12c70f95d24e682fbe29fcdec41fe79f1825f8
IFloresCh <94060293+IFloresCh@users.noreply.github.com>
2022-02-04 11:48:52 +0100

    Create main1.c
    primera corrección

---
+++ Revisiones/main1.c
@@ -0,0 +1,26 @@
+//https://github.com/ShiroSB/C-UF2/blob/main/act%2011
+
+#include <stdio.h>
+#define SIZE 100
+void evenNumbers();
+
+void main(){
+    printf("100 números pares generados proceduralmente:\n");
+    evenNumbers();
+   
+}
+
+void evenNumbers(){
+    int j=1,i=0,pares[SIZE];
+    for (i=0;i<=SIZE+1;i++){
+           pares[i]=j*2;
+           j++;
+    }
+    
+    for (i=0;i<=SIZE;i++){
+        printf("%d ",pares[i]);
+    
+    }
+}
+
+    
